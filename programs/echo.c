dc88e82b23ab3aa65afe246e90c3cbaf626fcc3b
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-06-23 09:28:01 -0400

    feat: make echo nolibc-compatible

--- programs/echo.c
+++ programs/echo.c
@@ -33,9 +33,11 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
+#ifndef NOLIBC
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#endif
 
 int
 main(int argc, const char *argv[])
