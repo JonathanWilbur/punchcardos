9e7114bbb38532708c0307f609f87bb92c2a7ede
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-06-26 13:27:27 -0400

    feat: make rm compile with nolibc

--- programs/rm.c
+++ programs/rm.c
@@ -1,7 +1,11 @@
+#ifndef NOLIBC
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#endif
 
+/* This does not support the -r option, because that would require a LOT more
+code. The -f option is implied. */
 int main (int argc, char* argv[])
 {
 	if (argc == 1)
@@ -10,12 +14,15 @@ int main (int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
+    #ifndef NOLIBC
 	if (access(argv[1], F_OK) != 0)
     { // The file does not exist.
 		perror("rm");
         return EXIT_FAILURE;
     }
+    #endif
 
+    /* The unlink system call is how you delete a file on Linux. */
     if (unlink(argv[1]) != 0)
     {
         perror("rm");
