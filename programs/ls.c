226e9d7da83c852517159e934b25f466a0a94f51
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-06-25 23:46:56 -0400

    feat: port ls to nolibc

--- programs/ls.c
+++ programs/ls.c
@@ -1,79 +1,99 @@
+#ifndef NOLIBC
+// This means "yes, I want access to non-portable features of glibc"
+#define _GNU_SOURCE
+
 #include <stdio.h>
-#include <dirent.h>
-#include <errno.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <dirent.h>
 
-int ls(const char *dir,int op_a,int op_l)
+struct linux_dirent64 {
+    ino64_t        d_ino;    /* 64-bit inode number */
+    off64_t        d_off;    /* Not an offset; see getdents() */
+    unsigned short d_reclen; /* Size of this dirent */
+    unsigned char  d_type;   /* File type */
+    char           d_name[]; /* Filename (null-terminated) */
+};
+
+#endif
+
+#define DIRENT_BUFSIZ   32 * sizeof(struct linux_dirent64)
+
+const char *USAGE_MSG = "Usage: ls [-l|-a] [path]";
+
+// TODO: Different exit failure codes.
+
+int ls (const char *dir, int op_a, int op_l)
 {
-	struct dirent *d;
-	DIR *dir_list = opendir(dir);
-	if (!dir_list)
-	{
-		if (errno = ENOENT)
-		{
-			perror("No such directory");
-		}
-		else
-		{
-            // TODO: Is there some way you can print the error message?
-			perror("Unable to read directory");
-		}
-		exit(EXIT_FAILURE);
-	}
+    int fd;
+    char dirbuf[DIRENT_BUFSIZ];
+    size_t bytes_read;
+    struct linux_dirent64 *d;
 
-    /* Every time you call readdir on the list, it returns the next directory
-    entry, or NULL if there are no more. */
-	while ((d = readdir(dir_list)) != NULL)
-	{
-		// Skip over hidden files.
-		if (!op_a && d->d_name[0] == '.')
-        {
-            continue;
+    fd = open(dir, O_RDONLY);
+    if (fd == -1)
+        return EXIT_FAILURE;
+    while (1) {
+        d = (struct linux_dirent64 *)&dirbuf;
+        bytes_read = getdents64(fd, d, DIRENT_BUFSIZ);
+        if (bytes_read <= 0) { // 0 means we're done, negative means failure.
+            break;
         }
-		printf("%s  ", d->d_name);
-		if (op_l)
-        {
-            printf("\n");
+        for (size_t byte_pos = 0; byte_pos < bytes_read; ) {
+            d = (struct linux_dirent64 *) (dirbuf + byte_pos);
+            byte_pos += d->d_reclen;
+            if (!op_a && d->d_name[0] == '.') {
+                continue;
+            }
+            // TODO: directory highlighting?
+            if (printf("%s%s", d->d_name, op_l ? "\n" : "  ") < 0)
+                return EXIT_FAILURE;
         }
-	}
-	if (!op_l) {
-	    printf("\n");
     }
+    if (bytes_read < 0) {
+        return EXIT_FAILURE;
+    }
+    if (!op_l) {
+        puts(""); // Intentionally ignore error code.
+    }
+    // assert(rc == 0);
     return EXIT_SUCCESS;
 }
 
-// TODO: Include help option.
-int main(int argc, const char *argv[])
+int main (int argc, const char *argv[])
 {
-	if (argc == 1)
-	{
+    int op_a = 0, op_l = 0;
+    char *dir = ".";
+
+	if (argc == 1) {
 		return ls(".", 0, 0);
 	}
-	else if (argc == 2)
-	{
-		if (argv[1][0] == '-')
-		{
-			int op_a = 0, op_l = 0;
-			char *p = (char*)(argv[1] + 1);
-			while(*p) {
-				if (*p == 'a')
-                {
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--help") == 0) {
+            if (puts(USAGE_MSG) == EOF)
+                return EXIT_FAILURE;
+            return EXIT_SUCCESS;
+        }
+        if (argv[i][0] == '-') {
+            char *p = (char*)(argv[1] + 1);
+            while (*p) {
+                if (*p == 'a') {
                     op_a = 1;
                 }
-				else if (*p == 'l')
-                {
+                else if (*p == 'l') {
                     op_l = 1;
                 }
-				else
-                {
-					perror("Unrecognized option");
-					exit(EXIT_FAILURE);
-				}
-				p++;
-			}
-			return ls(".", op_a, op_l);
-		}
-	}
-    // TODO: Handle multiple arguments.
-	return EXIT_SUCCESS;
+                else {
+                    perror("Unrecognized option");
+                    return EXIT_FAILURE;
+                }
+                p++;
+            }
+        } else {
+            dir = (char *)argv[i];
+        }
+    }
+    return ls(dir, op_a, op_l);
 }
