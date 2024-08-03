39e9cc351fc944d3e5c23dbfb02e9d4a27d5393f
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-08-03 17:16:16 -0400

    feat: add try script

---
+++ scripts/try.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ex
+
+docker build . -t pcosbuild -f ./punchcardos.dockerfile
+sudo docker run --privileged pcosbuild bash -c 'mkdir m && mount boot m && cp bzImage init.cpio syslinux.cfg files/* m && umount m'
+CONTAINER_ID=$(docker ps --format 'table {{.ID}} {{.Image}}' -a | grep pcosbuild | head -n1 | cut -d ' ' -f1)
+docker cp $CONTAINER_ID:/build/boot pcos.img
+# qemu-system-x86_64 -drive file=pcos.img,format=raw,index=0 -m 1G
+docker cp $CONTAINER_ID:/working-kilo ./working-kilo
+docker cp $CONTAINER_ID:/broken-kilo ./broken-kilo
