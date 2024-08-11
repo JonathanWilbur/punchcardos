f19e72c6cd2453fa9ac430de637faae9d168c604
Jonathan M. Wilbur <jonathan@wilbur.space>
2024-08-11 16:25:35 -0400

    style: rm and rmdir

--- programs/rm.c
+++ programs/rm.c
@@ -8,23 +8,20 @@
 code. The -f option is implied. */
 int main (int argc, char* argv[])
 {
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		puts("usage: rm {file-path-to-delete}");
 		return EXIT_FAILURE;
 	}
 
     #ifndef NOLIBC
-	if (access(argv[1], F_OK) != 0)
-    { // The file does not exist.
+	if (access(argv[1], F_OK) != 0) { // The file does not exist.
 		perror("rm");
         return EXIT_FAILURE;
     }
     #endif
 
     /* The unlink system call is how you delete a file on Linux. */
-    if (unlink(argv[1]) != 0)
-    {
+    if (unlink(argv[1]) != 0) {
         perror("rm");
         return EXIT_FAILURE;
     }
--- programs/rmdir.c
+++ programs/rmdir.c
@@ -6,13 +6,11 @@
 
 int main (int argc,char* argv[])
 {
-	if (argc != 2)
-	{
+	if (argc != 2) {
 		puts("usage: rmdir {dir-to-delete}");
 		return EXIT_FAILURE;
 	}
-	if (rmdir(argv[1]))
-	{
+	if (rmdir(argv[1])) {
 		perror("rmdir");
         return EXIT_FAILURE;
 	}
